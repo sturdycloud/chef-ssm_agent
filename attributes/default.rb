cd0e1daf62a95b553b846b5844b384e31e10e4de
Keith Gable <keith@art19.com>
2020-09-18 14:10:57 -0700

    Update copyrights

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,7 @@
 #
 # Copyright:: 2017, Sturdy Networks
 # Copyright:: 2017, Jonathan Serafini
+# Copyright:: 2020, Keith Gable
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
